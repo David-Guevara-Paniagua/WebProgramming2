28a93d9fb3796d78c0b1ed4f31b97de948f1ed38
David-Guevara-Paniagua <145381289+David-Guevara-Paniagua@users.noreply.github.com>
2026-02-25 19:09:30 -0600

    Aniado comparar numeros

---
+++ CompararNumeros/compararNum.js
@@ -0,0 +1,6 @@
+let a = parseInt(prompt("Dame un numero entero"));
+let b = parseInt(prompt("Dame otro numero entero"));
+
+if (a > b){console.log(a+" es mayor que "+b)}
+if (a < b){console.log(b+" es mayor que "+a)}
+if (a == b){console.log(a+" es igual que "+b)}
